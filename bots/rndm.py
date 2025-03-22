28884defb1d4aade4fe2eca61cf0942b910f09da
sanflan <fsandalinas@dc.uba.ar>
2025-03-22 15:34:09 -0300

    Fix random

--- bots/rndm.py
+++ bots/rndm.py
@@ -1,11 +1,19 @@
 import logging
 import random
+import unittest
+from collections import defaultdict
+from unittest.mock import Mock, patch
 from typing import List
 from game import Position, Terrain 
 from game import CASTLE, CONQUER, HARVEST, MINE, FORT, FARM, LAND
-import unittest
+
+logging.basicConfig(
+    filename="rndmdebug", level=logging.DEBUG,
+    filemode="w"
+)
 
 ALL_ACTIONS = [CASTLE, CONQUER, FARM, FORT]
+STRUCTURES = {CASTLE, FARM, FORT}
 
 def adj(p1: Position, p2: Position):
     x1, y1 = p1
@@ -25,7 +33,6 @@ def get_adj_tiles(world, tiles):
     ]
 
 def get_conquerable(adjacents, resources):
-    logging.info(adjacents)
     return [
         t for t in adjacents if can_conquer(t, resources)
     ]
@@ -42,59 +49,81 @@ def can_conquer(tile, resources):
         case 'land':
             return resources >= 1
 
-class BotLogic:
-    def turn(self, map_size, my_resources, world):
-        mine = get_my_tiles(world)
-        adjacent = get_adj_tiles(world, mine)
-        possible_actions = ALL_ACTIONS.copy()
-        action_type = random.choice(possible_actions)
-        
-        return self.make_move(
-            action_type, possible_actions,
-            mine, adjacent, world, my_resources
-            )
-            
+class World:
+    def __init__(self, world, resources):
+        self.resources = resources
+        self.mine = get_my_tiles(world)
+        self.adjacent = get_adj_tiles(world, self.mine)
+        self.conquerable = get_conquerable(self.adjacent, self.resources)
+        self.structures = defaultdict(list) 
+        for t in self.mine:
+            self.structures[t[1].structure].append(t)
 
-    def is_possible(
-        self, action, mine, adjacents, world, resources
+    def possible_positions(
+        self, action 
     ):
-        empty_land = [
-            t for t in mine if t[1].structure == LAND 
-        ]
-        if action in (FARM, FORT, CASTLE) and not empty_land:
+        empty_land = self.structures[LAND]
+        if action in STRUCTURES and not empty_land:
             return False
         match(action):
             case 'farm':
-                if resources >= 5:
+                if self.resources >= 5:
                     return empty_land
             case 'fort':
-                if resources >= 25:
+                if self.resources >= 25:
                     return empty_land
             case 'castle':
-                if resources >= 75:
+                if self.resources >= 75:
                     return empty_land
             case 'conquer':
-                return get_conquerable(adjacents, resources)
-                
-    def make_move(
-        self, action, possible_actions: List,
-        mine, adjacents, world, resources
-        ):
-        if not possible_actions or action == HARVEST:
-            return HARVEST, None
-        if positions := self.is_possible(
-            action, mine, adjacents, world, resources
-            ):
-            return action, random.choice(positions)[0]
-        else:
-            return self.make_move(
-                random.choice(possible_actions), possible_actions.remove(action),
-                mine, adjacents, world, resources
+                return self.conquerable
+        
+
+class BotLogic:
+    def turn(self, map_size, my_resources, world):
+        world = World(world, my_resources)
+        possible_actions = ALL_ACTIONS.copy()
+        return self.make_move(
+            possible_actions, world
             )
+
+    def choose_action(self, possible_actions):
+        return random.choice(possible_actions)
+
+    def choose_position(self, tiles):
+        return random.choice(tiles)[0]
+    
+    def make_move(
+        self, possible_actions: List, world: World):
+        if not possible_actions:
+            return HARVEST, None
+        action = self.choose_action(possible_actions)
+        if action == HARVEST:
+            return HARVEST, None
+        if tiles := world.possible_positions(action):
+            return action, self.choose_position(tiles)
+        else:
+            possible_actions.remove(action)
+            return self.make_move(possible_actions, world)
         raise Exception
 
 
-class TestAuxMethods(unittest.TestCase):
+class TestBot(unittest.TestCase):
+    other = "Terminal Khan"
+    khan_world = {
+            Position(1,1): Terrain(CASTLE, MINE),
+            Position(0,0): Terrain(CASTLE, other),
+            Position(0,1): Terrain(CASTLE, other),
+            Position(1,0): Terrain(CASTLE, other)
+        }
+
+    farm_vil = {
+            Position(1,1): Terrain(CASTLE, MINE),
+            Position(0,0): Terrain(LAND, MINE),
+            Position(0,1): Terrain(LAND, MINE),
+            Position(1,0): Terrain(LAND, MINE)
+        }
+    
     def test_adj(self):
         self.assertTrue(adj(Position(0,0), Position(0,1)))
         self.assertFalse(adj(Position(0,0), Position(1,1)))
@@ -109,29 +138,40 @@ class TestAuxMethods(unittest.TestCase):
         )
 
     def test_my_tiles(self):
-        other = "Terminal Khan"
-        world = {
-                Position(1,1): Terrain(CASTLE, MINE),
-                Position(0,0): Terrain(CASTLE, other),
-                Position(0,1): Terrain(CASTLE, other),
-                Position(1,0): Terrain(CASTLE, other)
-        }
-        self.assertEqual(get_my_tiles(world),
+        self.assertEqual(get_my_tiles(self.khan_world),
                          [
                              (Position(1,1), Terrain(CASTLE, MINE)),
                          ])
 
     def test_adjs(self):
-        other = "Terminal Khan"
-        world = {
-                Position(1,1): Terrain(CASTLE, MINE),
-                Position(0,0): Terrain(CASTLE, other),
-                Position(0,1): Terrain(CASTLE, other),
-                Position(1,0): Terrain(CASTLE, other)
-        }
+        world = self.khan_world
         self.assertEqual(set(get_adj_tiles(world, get_my_tiles(world))),
                          set([
-                            (Position(0,1), Terrain(CASTLE, other)),
-                            (Position(1,0), Terrain(CASTLE, other))
+                            (Position(0,1), Terrain(CASTLE, self.other)),
+                            (Position(1,0), Terrain(CASTLE, self.other))
                          ]))
+
+    @patch.object(BotLogic, 'choose_position', lambda x, y: Position(1, 0))
+    def test_conquer_khan(self):
+        actions = [CONQUER, FORT, FARM]
+        def choose(who, cares):
+            return actions.pop()
+        with patch.object(BotLogic, 'choose_action', choose):
+            bot = BotLogic()
+            self.assertEqual(
+                bot.turn((2,2), 100, self.khan_world),
+                (CONQUER, Position(1,0))
+            )
+
+    def test_farming_Jin(self):
+        bot = BotLogic()
+        res = bot.turn((2,2), 10, self.farm_vil)
+        self.assertEqual(
+             res[0], FARM 
+        )
+        self.assertIn(
+            res[1],
+            {Position(0,1), Position(1,0)}
+        )
+            
         
