a05eadc5df63e08e8fc8ada4aa26e5b63f2fb3a0
Juan Pedro Fisanotti <fisadev@gmail.com>
2025-03-22 14:06:30 -0300

    Ability to specify initial positions

--- toe.py
+++ toe.py
@@ -10,7 +10,7 @@ from ui import ToEUI
 @click.command()
 @click.option("--width", type=int, default=40, help="The width of the map.")
 @click.option("--height", type=int, default=20, help="The height of the map.")
-@click.option("--players", type=str, help="Players, specified as a comma separated list of player_name:bot_type.")
+@click.option("--players", type=str, help="Players, specified as a comma separated list of player_name:bot_type (or optionally with the initial position as player_name:bot_type:x.y) .")
 @click.option("--no-ui", is_flag=True, help="Don't show the ui, just run the game until the end and inform the winner.")
 @click.option("--ui-turn-delay", type=float, default=0.2, help="Seconds to wait between turns when showing the ui.")
 @click.option("--turn-timeout", type=float, default=0.5, help="Maximum seconds a player can take to think its turn.")
@@ -28,13 +28,23 @@ def main(width, height, players, no_ui, ui_turn_delay, log_path, turn_timeout):
 
     for player_info in players.split(","):
         try:
-            name, bot_type = player_info.split(":")
+            parts = player_info.split(":")
+            if len(parts) == 2:
+                name, bot_type = player_info.split(":")
+                castle_position = None
+            elif len(parts) == 3:
+                name, bot_type, position = player_info.split(":")
+                x, y = position.split(".")
+                castle_position = (int(x), int(y))
+            else:
+                raise ValueError()
+
             bot_type = bot_type.lower()
         except ValueError:
             print(f"Invalid player info: {player_info}. Should be name:bot_type")
             sys.exit(1)
 
-        toe.add_player(name, bot_type)
+        toe.add_player(name, bot_type, castle_position=castle_position)
 
     if ui:
         with ui.show():
