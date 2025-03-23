3d042e621a292fda9d9556088a65aaeed57cdb80
Sofía Denner <sofi.denner@gmail.com>
2025-03-23 03:05:53 -0300

    commiteo porque parece que funciona

--- bots/haciendo_camino.py
+++ bots/haciendo_camino.py
@@ -16,6 +16,9 @@ from game import (
     TILES_PER_CASTLE_LIMIT
 )
 
+def random_choice_from_set(my_set):
+    return random.choice(list(my_set))
+
 
 def is_adjacent(position1, position2):
     """
@@ -43,6 +46,7 @@ def adjacents(position, map_size):
 
 @dataclass
 class Insights:
+    enemies: set
     tiles_by_type_and_owner: dict
     forts_and_castles: set
     my_tiles: set
@@ -67,7 +71,7 @@ class Strategy:
         my_tiles = len(self.insights.my_tiles)
 
         size_enough = my_tiles / my_castles + 1 >= TILES_PER_CASTLE_LIMIT
-        money_enough = self.my_resources >= STRUCTURE_COST
+        money_enough = self.my_resources >= STRUCTURE_COST[CASTLE]
 
         if size_enough and money_enough:
             # replace a random fort, land or farm
@@ -76,40 +80,127 @@ class Strategy:
             my_farms = self.insights.tiles_by_type_and_owner[FARM][MINE]
 
             if my_forts:
-                return CASTLE, random.choice(my_forts)
+                return CASTLE, random_choice_from_set(my_forts)
             elif my_lands:
-                return CASTLE, random.choice(my_lands)
+                return CASTLE, random_choice_from_set(my_lands)
             elif my_farms:
-                return CASTLE, random.choice(my_farms)
+                return CASTLE, random_choice_from_set(my_farms)
 
         return None
 
+    def decide_to_fortify(self):
+        if self.my_resources < STRUCTURE_COST[FORT]:
+            return False
+
+        if not self.insights.unprotected_terrain:
+            return False
+
+        max_enemy_forts = max(
+            len(self.insights.tiles_by_type_and_owner[FORT][enemy])
+            for enemy in self.insights.enemies
+        )
+        my_forts = len(self.insights.tiles_by_type_and_owner[FORT][MINE])
+
+        # fortify if someone has more forts than me or bc 20% random
+        if my_forts < max_enemy_forts or random.random() > 0.2:
+            return True
+
+        return False
+
+
+    def fortify(self):
+        fortification_max_benefit = max(self.insights.where_to_fort)
+
+        position_to_fortify = self.insights.where_to_fort[fortification_max_benefit]
+
+        return FORT, position_to_fortify
+
+    def decide_to_farm(self):
+        if self.my_resources < STRUCTURE_COST[FARM]:
+            return False
+
+        if not self.insights.tiles_by_type_and_owner[LAND][MINE]:
+            return False
+
+        max_enemy_farms = max(
+            len(self.insights.tiles_by_type_and_owner[FARM][enemy])
+            for enemy in self.insights.enemies
+        )
+        my_farms = len(self.insights.tiles_by_type_and_owner[FARM][MINE])
+
+        # build farms if someone has more farms than me or bc 20% random
+        if my_farms < max_enemy_farms or random.random() <= 0.2:
+            return True
+
+        return False
+
+    def farm(self):
+
+        if self.insights.where_to_farm:
+            position_to_farm = random_choice_from_set(self.insights.where_to_farm)
+        else:
+            all_my_lands = self.insights.tiles_by_type_and_owner[LAND][MINE]
+            position_to_farm = random_choice_from_set(all_my_lands)
+
+        return FARM, position_to_farm
+
+    def decide_to_conquer(self):
+        can_afford = any(cost for cost in self.insights.where_to_expand.keys() if cost <= self.my_resources)
+        costs = [cost for cost in self.insights.where_to_expand.keys()]
+        resources = self.my_resources
+        logging.warning("AFFORD %s", can_afford)
+        logging.warning("costs %s", costs)
+        logging.warning("resources %s", resources)
+        logging.warning("WHERE TO EXPAND %s", self.insights.where_to_expand)
+
+        if can_afford and random.random() <= 0.8:
+            return True
+
+        return False
+
+    def conquer(self):
+        max_affordable_tile = max(cost for cost in self.insights.where_to_expand.keys() if cost <= self.my_resources)
+
+        position_to_conquer = random_choice_from_set(self.insights.where_to_expand[max_affordable_tile])
+
+        return CONQUER, position_to_conquer
 
     def select_action(self):
+        logging.warning("ACTION")
         if not self.my_resources:
+            logging.warning("NO RESOURCES %s", self.my_resources)
             return self.harvest()
 
         build_castle = self.build_castle()
-
         if build_castle:
             return build_castle
+        logging.warning("no castle")
 
+        if self.decide_to_fortify():
+            return self.fortify()
+        logging.warning("no fortify")
 
+        if self.decide_to_farm():
+            return self.farm()
+        logging.warning("no farm")
 
+        if self.decide_to_conquer():
+            return self.conquer()
+        logging.warning("no conquer")
 
-
-
-
+        return self.harvest()
 
 
 class BotLogic:
 
     @staticmethod
-    def _get_tiles_by_type_and_owner(world):
-        _tiles_by_type_and_owner = defaultdict(lambda: defaultdict(set))
+    def _get_enemies_and_tiles_by_type_and_owner(world):
+        enemies = set()
+        tiles_by_type_and_owner = defaultdict(lambda: defaultdict(set))
         for position, terrain in world.items():
-            _tiles_by_type_and_owner[terrain.structure][terrain.owner].add(position)
-        return _tiles_by_type_and_owner
+            tiles_by_type_and_owner[terrain.structure][terrain.owner].add(position)
+            enemies.add(terrain.owner)
+        return enemies, tiles_by_type_and_owner
 
     @staticmethod
     def _get_my_tiles(tiles_by_type_and_owner):
@@ -181,8 +272,8 @@ class BotLogic:
 
     @staticmethod
     def _get_conquer_cost(tile, world, tiles_by_type_and_owner, map_size):
-        oponent = world[tile].owner
-        structure = world[tile].structure
+        oponent = world[tuple(tile)].owner
+        structure = world[tuple(tile)].structure
 
         oponent_castles_and_forts = tiles_by_type_and_owner[CASTLE][oponent].union(tiles_by_type_and_owner[FORT][oponent])
 
@@ -208,17 +299,20 @@ class BotLogic:
 
         for tile in my_tiles:
             adjacent_positions = adjacents(tile, map_size)
+            logging.warning("ADJACENTS %s", adjacent_positions)
 
             not_mine = adjacent_positions - my_tiles
+            logging.warning(" MINE %s", my_tiles)
+            logging.warning("NOT MINE %s", not_mine)
             for not_my_tile in not_mine:
-                cost = self._get_conquer_cost(tile, world, tiles_by_type_and_owner, map_size)
+                cost = self._get_conquer_cost(not_my_tile, world, tiles_by_type_and_owner, map_size)
 
                 where_to_expand[cost].add(not_my_tile)
 
         return where_to_expand
 
     def process_world(self, world, map_size):
-        tiles_by_type_and_owner = self._get_tiles_by_type_and_owner(world)
+        enemies, tiles_by_type_and_owner = self._get_enemies_and_tiles_by_type_and_owner(world)
         forts_and_castles = tiles_by_type_and_owner[CASTLE][MINE].union(tiles_by_type_and_owner[FORT][MINE])
         my_tiles = self._get_my_tiles(tiles_by_type_and_owner)
         protected_terrain, unprotected_terrain = self._get_protected_and_uprotected_tiles(my_tiles, forts_and_castles, map_size)
@@ -228,6 +322,7 @@ class BotLogic:
         where_to_expand = self._get_where_to_expand(my_tiles, world, tiles_by_type_and_owner, map_size)
 
         return Insights(
+            enemies,
             tiles_by_type_and_owner,
             forts_and_castles,
             my_tiles,
@@ -243,6 +338,12 @@ class BotLogic:
         logging.warning("TURNNNNNN")
         try:
             insights = self.process_world(world, map_size)
+            strategy = Strategy(insights, my_resources)
+            action = strategy.select_action()
+            if not isinstance(action, tuple):
+                raise Exception()
+
+            return action
         except:
-            logging.exception("ERRRRORRRR")
+            logging.exception("Error, use default strategy")
         return "harvest", None
